9c2c6a7629f71e2fbe8cc8fa6d0d35d30c924a24
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-03 00:35:15 +0530

    Added annotations for path variables and request parameters and changed url mapping of similar urls

--- src/main/java/com/cricketSeries/controller/CricketController.java
+++ src/main/java/com/cricketSeries/controller/CricketController.java
@@ -7,15 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cricketSeries.model.Series;
 import com.cricketSeries.dto.MatchDTO;
 import com.cricketSeries.dto.SeriesRequestDTO;
+import com.cricketSeries.dto.SeriesResponseDTO;
 import com.cricketSeries.model.Match;
 import com.cricketSeries.service.CricketService;
 
@@ -37,48 +40,48 @@ public class CricketController {
 		return service.getAllSeries();
 	}
 
-	@GetMapping("/series")
-	public Series getSeriesById(Long id) {
+	@GetMapping("/series/{id}")
+	public SeriesResponseDTO getSeriesById(@PathVariable Long id) {
 		return service.getSeriesById(id);
 	}
 
 	@DeleteMapping("/series")
-	public String deleteSeries(Long id) {
+	public String deleteSeries(@RequestParam Long id) {
 		service.deleteSeries(id);
 		return "Series deleted with id: " + id;
 	}
 
 	@PutMapping("/series")
-	public String updateSeries(Series series) {
+	public String updateSeries(@RequestBody Series series) {
 		service.updateSeries(series);
 		return "Series updated with id: " + series.getId();
 	}
 
-	@PostMapping("/match")
-	public ResponseEntity<String> createMatch(Long id, @RequestBody MatchDTO match) {
+	@PostMapping("/match/{id}")
+	public ResponseEntity<String> createMatch(@PathVariable Long id, @RequestBody MatchDTO match) {
 		service.createMatch(id, match);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Created");
 	}
 
-	@GetMapping("/match")
-	public List<Match> getAllMatchesBySeriesId(Long id) {
+	@GetMapping("/matches/{id}")
+	public List<MatchDTO> getAllMatchesBySeriesId(@PathVariable Long id) {
 		return service.getAllMatchesBySeriesId(id);
 	}
 
-	@GetMapping("/match")
-	public Match getMatchById(Long id) {
+	@GetMapping("/match/{id}")
+	public MatchDTO getMatchById(@PathVariable Long id) {
 		return service.getMatchById(id);
 	}
 
 	@DeleteMapping("/match")
-	public String deleteMatch(Long id) {
+	public String deleteMatch(@RequestParam Long id) {
 		service.deleteMatch(id);
 		return "Match deleted with id: " + id;
 	}
 
-	@PutMapping("/match")
-	public String updateMatch(Match match) {
-		service.updateMatch(match);
+	@PutMapping("/match/{id}")
+	public String updateMatch(@PathVariable Long id, @RequestBody MatchDTO match) {
+		service.updateMatch(id, match);
 		return "Match updated with id: " + match.getId();
 	}
 
