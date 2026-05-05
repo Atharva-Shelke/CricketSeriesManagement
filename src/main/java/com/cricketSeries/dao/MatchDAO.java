2f43aaa575b562cfbb9168fc1867501521afbde5
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-05 20:57:48 +0530

    Modified vague variable names to meaningful and self explanatory names

--- src/main/java/com/cricketSeries/dao/MatchDAO.java
+++ src/main/java/com/cricketSeries/dao/MatchDAO.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,21 +20,21 @@ public class MatchDAO {
 	@Autowired
 	private SqlLoader sql;
 
-	public int insertMatch(Long seriesId, Match match) {
+	public int insertMatch(long seriesId, Match match) {
 		String query = sql.get("insert_match");
 
 		MapSqlParameterSource params = new MapSqlParameterSource().addValue("series_id", seriesId)
 				.addValue("team_a", match.getTeamA()).addValue("team_b", match.getTeamB())
-				.addValue("match_date", match.getMatchDate()).addValue("venue", match.getVenue())
+				.addValue("match_date", Date.valueOf(match.getMatchDate())).addValue("venue", match.getVenue())
 				.addValue("match_type", match.getMatchType());
 
 		return jdbcTemplate.update(query, params);
 
 	}
 
-	public List<Match> getAllMatchesBySeriesId(Long id) {
+	public List<Match> getMatchesBySeriesId(long seriesId) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", seriesId);
 			return jdbcTemplate.query(sql.get("get_all_matches_by_series_id"), params, (rs, rowNum) -> {
 				Match match = new Match();
 				match.setId(rs.getLong("id"));
@@ -44,19 +45,19 @@ public class MatchDAO {
 				match.setMatchType(rs.getString("match_type"));
 				return match;
 			});
-		} catch (RuntimeException ex) {
-			throw new RuntimeException("Series not found with id: " + id);
+		} catch (EmptyResultDataAccessException ex) {
+			throw new RuntimeException("Series not found with id: " + seriesId);
 		}
 
 	}
 
-	public Match getMatchById(Long id) {
+	public Match getMatchById(long matchId) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", matchId);
 
 			return jdbcTemplate.queryForObject(sql.get("get_match_by_id"), params, (rs, rowNum) -> {
 				Match match = new Match();
-				match.setId(id);
+				match.setId(matchId);
 				match.setTeamA(rs.getString("team_a"));
 				match.setTeamB(rs.getString("team_b"));
 				match.setMatchDate(rs.getDate("match_date").toLocalDate());
@@ -65,22 +66,22 @@ public class MatchDAO {
 				return match;
 			});
 		} catch (RuntimeException ex) {
-			throw new RuntimeException("Match not found with id: " + id);
+			throw new RuntimeException("Match not found with id: " + matchId);
 		}
 	}
 
-	public int deleteMatch(Long id) {
+	public int deleteMatch(long matchId) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", matchId);
 			return jdbcTemplate.update(sql.get("delete_match"), params);
 		} catch (RuntimeException ex) {
-			throw new RuntimeException("Match not found with id: " + id);
+			throw new RuntimeException("Match not found with id: " + matchId);
 		}
 	}
 
-	public int updateMatch(Long id, Match match) {
+	public int updateMatch(long matchId, Match match) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id)
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", matchId)
 					.addValue("team_a", match.getTeamA()).addValue("team_b", match.getTeamB())
 					.addValue("match_date", Date.valueOf(match.getMatchDate())).addValue("venue", match.getVenue())
 					.addValue("match_type", match.getMatchType());
--- src/main/java/com/cricketSeries/dao/SeriesDAO.java
+++ src/main/java/com/cricketSeries/dao/SeriesDAO.java
@@ -3,6 +3,7 @@ package com.cricketSeries.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -28,7 +29,7 @@ public class SeriesDAO {
 
 	private final ObjectMapper mapper = new ObjectMapper();
 
-	public Long insertSeries(Series series) {
+	public long insertSeries(Series series) {
 		String query = sql.get("insert_series");
 
 		MapSqlParameterSource params = new MapSqlParameterSource().addValue("name", series.getName())
@@ -40,7 +41,7 @@ public class SeriesDAO {
 		int rows = jdbcTemplate.update(query, params, keyHolder, new String[] { "id" });
 
 		if (rows == 0) {
-			throw new RuntimeException("Insert failed: no rows affected");
+			throw new RuntimeException("Series not inserted.");
 		}
 
 		return keyHolder.getKey().longValue();
@@ -72,12 +73,11 @@ public class SeriesDAO {
 			try {
 				String matchesJson = rs.getString("matches");
 
-				if (matchesJson != null) {
-					List<MatchResponseDTO> matches = mapper.readValue(matchesJson,
-							new TypeReference<List<MatchResponseDTO>>() {
-							});
-					series.setMatches(matches);
-				}
+				List<MatchResponseDTO> matches = mapper.readValue(matchesJson,
+						new TypeReference<List<MatchResponseDTO>>() {
+						});
+				series.setMatches(matches);
+
 			} catch (Exception ex) {
 				throw new RuntimeException("Error parsing matches JSON", ex);
 			}
@@ -87,10 +87,10 @@ public class SeriesDAO {
 
 	}
 
-	public Series getSeriesById(Long id) {
+	public Series getSeriesById(long seriesId) {
 		try {
 
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", seriesId);
 
 			return jdbcTemplate.queryForObject(sql.get("get_series_by_id"), params, (rs, rowNum) -> {
 				Series series = new Series();
@@ -101,14 +101,14 @@ public class SeriesDAO {
 				series.setEndDate(rs.getDate("end_date").toLocalDate());
 				return series;
 			});
-		} catch (RuntimeException ex) {
-			throw new RuntimeException("Series not found with id: " + id);
+		} catch (EmptyResultDataAccessException ex) {
+			throw new RuntimeException("Series not found with id: " + seriesId);
 		}
 	}
 
-	public SeriesResponseDTO getSeriesByIdWithMatches(Long id) {
+	public SeriesResponseDTO getSeriesByIdWithMatches(long seriesId) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", seriesId);
 
 			return jdbcTemplate.queryForObject(sql.get("get_series_by_id_with_matches"), params, (rs, rowNum) -> {
 				SeriesResponseDTO series = new SeriesResponseDTO();
@@ -132,17 +132,17 @@ public class SeriesDAO {
 				}
 				return series;
 			});
-		} catch (RuntimeException ex) {
-			throw new RuntimeException("Series not found with id: " + id);
+		} catch (EmptyResultDataAccessException ex) {
+			throw new RuntimeException("Series not found with id: " + seriesId);
 		}
 	}
 
-	public int deleteSeries(Long id) {
+	public int deleteSeries(long seriesId) {
 		try {
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+			MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", seriesId);
 			return jdbcTemplate.update(sql.get("delete_series"), params);
 		} catch (RuntimeException ex) {
-			throw new RuntimeException("Series not found with id: " + id);
+			throw new RuntimeException("Series not found with id: " + seriesId);
 		}
 	}
 
